144a106a6f329f39d4b4f26b480f9ddb780182e4
Tanner Olheiser <tolheiser@outlook.com>
2023-04-22 11:18:08 -0600

    Data

---
+++ src/util/cityJSON.js
@@ -0,0 +1,680 @@
+Assiniboia:
+
+{
+    "address": "201 3rd Avenue West",
+    "address2": "Prince of Wales Cultural & Recreational Centre",
+    "postal_code": "S0H 0B0",
+    "city": "Assiniboia",
+    "province": "Saskatchewan",
+    "phone": "3066943612",
+    "fax": "3066943043"
+}
+
+Broadview:
+
+{
+    "address": "715 Main Street",
+    "address2": "Community Hall",
+    "postal_code": "S4R 1W9",
+    "city": "Broadview",
+    "province": "Saskatchewan",
+    "phone": "3067861400",
+    "fax": "3067861422"
+}
+
+Carlyle:
+
+{
+    "address": "102 Main Street",
+    "address2": "Civic Centre",
+    "postal_code": "",
+    "city": "Carlyle",
+    "province": "Saskatchewan",
+    "phone": "3066374528",
+    "fax": "3066374536"
+}
+
+Canora:
+
+{
+    "address": "418 Main Street",
+    "address2": "Canora Town Office Building",
+    "postal_code": "",
+    "city": "Canora",
+    "province": "Saskatchewan",
+    "phone": "3067861400",
+    "fax": "3067861422"
+}
+
+Esterhazy:
+
+{
+    "address": "800 4th Avenue",
+    "address2": "S.N. Boreen Community Centre",
+    "postal_code": "",
+    "city": "Esterhazy",
+    "province": "Saskatchewan",
+    "phone": "3067861400",
+    "fax": "3067861422"
+}
+
+Indian Head:
+
+{
+    "address": "423 Grand Avenue",
+    "address2": "Memorial Hall",
+    "postal_code": "",
+    "city": "Indian Head",
+    "province": "Saskatchewan",
+    "phone": "3067875250",
+    "fax": "3067877037"
+}
+
+Kamsack:
+
+{
+    "address": "241 2nd Street",
+    "address2": "Crowstand Centre",
+    "postal_code": "",
+    "city": "Kamsack",
+    "province": "Saskatchewan",
+    "phone": "3067861400",
+    "fax": "3067861422"
+}
+
+Leader:
+
+{
+    "address": "402 1st Avenue W",
+    "address2": "Community Centre",
+    "postal_code": "",
+    "city": "Leader",
+    "province": "Saskatchewan",
+    "phone": "3067788390",
+    "fax": "3067788581"
+}
+
+Maple Creek:
+
+{
+    "address": "218 Maple Street",
+    "address2": "Legion Hall",
+    "postal_code": "",
+    "city": "Maple Creek",
+    "province": "Saskatchewan",
+    "phone": "3067788390",
+    "fax": "3067788581"
+}
+
+Moosomin:
+
+{
+    "address": "709 Carleton Street",
+    "address2": "Provincial Building",
+    "postal_code": "",
+    "city": "Moosomin",
+    "province": "SaskatchewanSK",
+    "phone": "3067861400",
+    "fax": "3067861422"
+}
+
+Outlook:
+
+{
+    "address": "400 Saskatchewan Avenue West",
+    "address2": "Town Hall",
+    "postal_code": "",
+    "city": "Outlook",
+    "province": "Saskatchewan",
+    "phone": "3069337052",
+    "fax": "3069337043"
+}
+
+Rosetown:
+
+{
+    "address": "814 Highway 4 North",
+    "address2": "Elks Lodge",
+    "postal_code": "",
+    "city": "Rosetown",
+    "province": "Saskatchewan",
+    "phone": "3069337052",
+    "fax": "3069337043"
+}
+
+Shaunavon:
+
+{
+    "address": "401 3rd Street West",
+    "address2": "Court House",
+    "postal_code": "",
+    "city": "Shaunavon",
+    "province": "Saskatchewan",
+    "phone": "3067788390",
+    "fax": "3067788581"
+}
+
+Wadena:
+
+{
+    "address": "254 Main Street North",
+    "address2": "Wadena Community Legion Hall",
+    "postal_code": "",
+    "city": "Wadena",
+    "province": "Saskatchewan",
+    "phone": "3065545521",
+    "fax": "3065545531"
+}
+
+Wakaw:
+
+{
+    "address": "211 Main Street",
+    "address2": "Wakaw Rec Centre",
+    "postal_code": "",
+    "city": "Wakaw",
+    "province": "Saskatchewan",
+    "phone": "3067526230",
+    "fax": "3067526126"
+}
+
+Wynard:
+
+{
+    "address": "Box 1449 – 410 Avenue C East",
+    "address2": "",
+    "postal_code": "S0A 4T0",
+    "city": "Wynyard",
+    "province": "Saskatchewan",
+    "phone": "3065545521",
+    "fax": "3065545531"
+}
+
+Beauval:
+
+{
+    "address": "Beaver Avenue",
+    "address2": "St. Mary Magdalene Catholic Church",
+    "postal_code": "",
+    "city": "Beauval",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Black Lake:
+
+{
+    "address": "118 Allsaints Street",
+    "address2": "Band Hall",
+    "postal_code": "",
+    "city": "Black Lake",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+Buffalo Narrows:
+
+{
+    "address": "310 Davy Street",
+    "address2": "Provincial Building",
+    "postal_code": "",
+    "city": "Buffalo Narrows",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Buffalo River Dene Nation:
+
+{
+    "address": "Band Hall",
+    "address2": "",
+    "postal_code": "",
+    "city": "Buffalo River Dene Nation",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Canoe Lake Cree First Nation:
+
+{
+    "address": "Band Hall",
+    "address2": "",
+    "postal_code": "",
+    "city": "Canoe Lake Cree First Nation",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Creighton:
+
+{
+    "address": "298 1st Street East",
+    "address2": "Provincial Building",
+    "postal_code": "",
+    "city": "Creighton",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+Cumberland House:
+
+{
+    "address": "19 Cumberland Street",
+    "address2": "Village Office",
+    "postal_code": "",
+    "city": "Cumberland House",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+Deschambault Lake:
+
+{
+    "address": "Band Office",
+    "address2": "Kistapiskaw Street",
+    "postal_code": "",
+    "city": "Deschambault Lake",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+Fond du Lac:
+
+{
+    "address": "Band Office",
+    "address2": "Gamache Street",
+    "postal_code": "",
+    "city": "Fond du Lac",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+Hudson Bay:
+
+{
+    "address": "304 Main Street",
+    "address2": "Town Hall",
+    "postal_code": "",
+    "city": "Hudson Bay",
+    "province": "Saskatchewan",
+    "phone": "3067526230",
+    "fax": "3067526126"
+}
+
+Ile a la Crosse:
+
+{
+    "address": "P10 Lajeunesse Avenue",
+    "address2": "Provincial Building",
+    "postal_code": "",
+    "city": "Ile a la Crosse",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+La Loche:
+
+{
+    "address": "La Loche Avenue",
+    "address2": "Provincial Building",
+    "postal_code": "",
+    "city": "La Loche",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Montreal Lake:
+{
+    "address": "Band Hall",
+    "address2": "Montreal Lake Cree Nation",
+    "postal_code": "",
+    "city": "Montreal Lake",
+    "province": "Saskatchewan",
+    "phone": "3069532640",
+    "fax": "3069532819"
+}
+
+Pelican Narrows:
+
+{
+    "address": "Band Hall",
+    "address2": "#1 PCLP Bear Street",
+    "postal_code": "",
+    "city": "Pelican Narrows",
+    "province": "Saskatchewan",
+    "phone": "3069532640",
+    "fax": "3069532819"
+}
+
+Pinehouse:
+
+{
+    "address": "L.C.A. Hall",
+    "address2": "Hilltop Avenue",
+    "postal_code": "",
+    "city": "Pinehouse",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+Sandy Bay:
+
+{
+    "address": "Gary Morin Memorial Hall",
+    "address2": "",
+    "postal_code": "",
+    "city": "Sandy Bay",
+    "province": "Saskatchewan",
+    "phone": "3069532640",
+    "fax": "3069532819"
+}
+
+Southend:
+
+{
+    "address": "Band Hall",
+    "address2": "Block 2, Lots 20 and 21, Lagoon Road",
+    "postal_code": "",
+    "city": "Southend",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+St. Walburg:
+
+{
+    "address": "521 4th Street East",
+    "address2": "Elk’s Hall",
+    "postal_code": "",
+    "city": "St. Walburg",
+    "province": "Saskatchewan",
+    "phone": "3068256585",
+    "fax": "3068256497"
+}
+
+Turnor Lake:
+
+{
+    "address": "10 Young Street",
+    "address2": "Skating Rink (Upstairs)",
+    "postal_code": "",
+    "city": "Turnor Lake",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Wollaston Lake:
+
+{
+    "address": "Senator Angus Tsannie Memorial Centre",
+    "address2": "North Point",
+    "postal_code": "",
+    "city": "Wollaston Lake",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+
+skipHere
+
+Ahtahkakoop Cree Nation:
+{
+    "address": "Old Band Office",
+    "address2": "Building #4",
+    "postal_code": "",
+    "city": "Ahtahkakoop Cree Nation",
+    "province": "Saskatchewan",
+    "phone": "3069532640",
+    "fax": "3069532819"
+}
+
+Beardy's and Okemasis' Cree Nation: {
+    "address": "Beardy’s Justice Building",
+    "address2": "",
+    "postal_code": "",
+    "city": "Beardy's and Okemasis' Cree Nation",
+    "province": "Saskatchewan",
+    "phone": "3069337052",
+    "fax": "3069337043"
+}
+
+Biggar: {
+    "address": "319 1st Ave. East",
+    "address2": "Biggar Community Hall",
+    "postal_code": "",
+    "city": "Biggar",
+    "province": "Saskatchewan",
+    "phone": "3064467400",
+    "fax": "3064467432"
+}
+
+Blaine Lake: {
+    "address": "2nd Floor, 218 2nd Avenue East",
+    "address2": "Blaine Lake Curling Club",
+    "postal_code": "",
+    "city": "Blaine Lake",
+    "province": "Saskatchewan",
+    "phone": "3069337052",
+    "fax": "3069337043"
+}
+
+Carrot River: {
+    "address": "3240 3rd Avenue",
+    "address2": "Carrot River Community Hall",
+    "postal_code": "",
+    "city": "Carrot River",
+    "province": "Saskatchewan",
+    "phone": "3067526230",
+    "fax": "3067526126"
+}
+
+Cut Knife:{
+    "address": "204 Otter Street",
+    "address2": "Curling Rink",
+    "postal_code": "",
+    "city": "Cut Knife",
+    "province": "Saskatchewan",
+    "phone": "3064467400",
+    "fax": "3064467432"
+}
+
+English River First Nation: {
+    "address": "Band Hall",
+    "address2": "",
+    "postal_code": "",
+    "city": "English River First Nation",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+--------------------------
+Humboldt: {
+    "address": "805 8th Avenue",
+    "address2": "Court House",
+    "postal_code": "",
+    "city": "Humboldt",
+    "province": "Saskatchewan",
+    "phone": "3069337052",
+    "fax": "3069337043"
+}
+
+Loon Lake: {
+    "address": "Recreational Centre",
+    "address2": "First Street South",
+    "postal_code": "",
+    "city": "Loon Lake",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+Nipawin: {
+    "address": "210 1st Street East",
+    "address2": "McNevin Building",
+    "postal_code": "S3N 0H6",
+    "city": "Nipawin",
+    "province": "Saskatchewan",
+    "phone": "3067526230",
+    "fax": "3067526126"
+}
+
+Onion Lake Cree Nation:
+
+"Melfort": {
+    "address": "Box 4480 – 107 Crawford Avenue East",
+    "address2": "",
+    "postal_code": "S0E 1A0",
+    "city": "Melfort",
+    "province": "Saskatchewan",
+    "phone": "3067526230",
+    "fax": "3067526126"
+}
+
+"Meadow Lake": {
+    "address": "207 3rd Avenue East",
+    "address2": "",
+    "postal_code": "S9X 1E7",
+    "city": "Meadow Lake",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598"
+}
+
+"Lloydminster": {
+    "address": "4815 – 50th Street",
+    "address2": "",
+    "postal_code": "S9V 0M8",
+    "city": "Lloydminster",
+    "province": "Saskatchewan",
+    "phone": "3068256585",
+    "fax": "3068256497"
+}
+
+"Pierceland": {
+    "address": "First Avenue South",
+    "address2": "Community Hall",
+    "postal_code": "",
+    "city": "Pierceland",
+    "province": "Saskatchewan",
+    "phone": "3062367575",
+    "fax": "3062367598",
+}
+
+"Punnichy": {
+    "address": "609 6th Avenue",
+    "address2": "Community Hall",
+    "postal_code": "",
+    "city": "Punnichy",
+    "province": "Saskatchewan",
+    "phone": "3065545521",
+    "fax": "3065545531",
+}
+
+"Rose Valley": {
+    "address": "316 1st Avenue North",
+    "address2": "Community Centre",
+    "postal_code": "",
+    "city": "Rose Valley",
+    "province": "Saskatchewan",
+    "phone": "3065545521",
+    "fax": "3065545531"
+}
+
+
+"Rosthern": {
+    "address": "2008 6th Street",
+    "address2": "Lions Hall",
+    "postal_code": "",
+    "city": "Rosthern",
+    "province": "Saskatchewan",
+    "phone": "3069337052",
+    "fax": "3069337043",
+}
+
+
+"Shellbrook": {
+    "address": "103 Railway Avenue",
+    "address2": "Shellbrook Community Hall",
+    "postal_code": "",
+    "city": "Shellbrook",
+    "province": "Saskatchewan",
+    "phone": "3069532640",
+    "fax": "3069532819",
+}
+
+"Spiritwood": {
+    "address": "303A 1st Street East",
+    "address2": "Civic Centre",
+    "postal_code": "",
+    "city": "Spiritwood",
+    "province": "Saskatchewan",
+    "phone": "3064467400",
+    "fax": "3064467432",
+}
+
+"Stanley Mission": {
+    "address": "643 Metos Street",
+    "address2": "Hamlet Hall",
+    "postal_code": "",
+    "city": "Stanley Mission",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269",
+}
+
+"Unity": {
+    "address": "312 Main Street",
+    "address2": "Legion Hall",
+    "postal_code": "",
+    "city": "Unity",
+    "province": "Saskatchewan",
+    "phone": "3064467400",
+    "fax": "3064467432",
+}
+
+Whitefish (Big River) First Nation:
+{
+    "address": "Multi-Purpose Building",
+    "address2": "",
+    "postal_code": "",
+    "city": "Whitefish (Big River) First Nation",
+    "province": "Saskatchewan",
+    "phone": "3069532640",
+    "fax": "3069532819",
+}          
+
+------
+
+{
+    "address": "Box 117 – 1320 La Ronge Avenue",
+    "address2": "",
+    "postal_code": "",
+    "city": "La Ronge",
+    "province": "Saskatchewan",
+    "phone": "3064254505",
+    "fax": "3064254269"
+}
+{
+    "address": "177 Segwun Avenue",
+    "address2": "Provincial Building",
+    "postal_code": "",
+    "city": "Fort Qu’Appelle",
+    "province": "Saskatchewan",
+    "phone": "3067875250",
+    "fax": "3067877037",
+}
+
