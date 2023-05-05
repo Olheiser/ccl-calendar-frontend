b9a5cdab25ad6ec4e74084422ba3ba1fb6294ec4
Tanner Olheiser <tolheiser@outlook.com>
2023-05-05 15:41:48 -0600

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false,
-  content: [],
+  purge: {
+    content: [
+      './src/**/*.html',
+      './src/**/*.js',
+      // Add more paths if needed
+    ],
+  },
   theme: {
     extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
-}
-
+};
