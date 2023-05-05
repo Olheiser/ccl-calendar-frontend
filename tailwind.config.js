3a7f4e268d3fc95b3e132d06f165e66576f7970b
Tanner Olheiser <tolheiser@outlook.com>
2023-05-05 16:18:57 -0600

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: [
-      './src/**/*.html',
-      './src/**/*.js',
-      // Add more paths if needed
-    ],
-  },
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    // Add more paths if needed
+  ],
   theme: {
     extend: {},
   },
